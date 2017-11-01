fd99095a076dbd42c6824249552ead75fa9061c9
Wilson Zheng <ZhengW@ad.fronde.com>
2017-11-02 12:13:41 +1300

    update

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ var http = require("http");
 var express = require("express");
 var app = express();
 
-//setup the view engine(Jade)
-app.set("view engine","jade");
+//--------------Jade example----------------------------
+// app.set("view engine","jade");//setup the view engine(Jade)
+// app.get("/",function(req,res){
+//     res.render("jade/index",{title:"Express + Jade"});
+// });
 
+//----------------EJS example----------------------------
+
+app.set("view engine","ejs");//ejs view engine
 app.get("/",function(req,res){
-    res.render("jade/index",{title:"Express + Jade"});
+    res.render("ejs/index",{title:"Express + EJS"});
 });
+//-------------------------------------------------------
 
-app.get("/api/users",function(req,res){
-    res.set("Content-Type","application/json");
-    res.send({name:"Wilson",isValid:true,group:"Admin"});
-});
-
+// app.get("/api/users",function(req,res){
+//     res.set("Content-Type","application/json");
+//     res.send({name:"Wilson",isValid:true,group:"Admin"});
+// });
 var server = http.createServer(app);
 server.listen(3000);
